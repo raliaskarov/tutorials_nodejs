35ea3b28e2c99fc5565c9782a23ae59a0f81c81c
raliaskarov <r.aliaskarov@gmail.com>
2025-05-02 08:58:43 +0200

    final project started

---
+++ expressBookReviews/final_project/router/booksdb.js
@@ -0,0 +1,66 @@
+// router/booksdb.js
+const books = [
+      {
+        isbn: "1",
+        author: "Chinua Achebe",
+        title: "Things Fall Apart",
+        reviews: {}
+      },
+      {
+        isbn: "2",
+        author: "Hans Christian Andersen",
+        title: "Fairy tales",
+        reviews: {}
+      },
+      {
+        isbn: "3",
+        author: "Dante Alighieri",
+        title: "The Divine Comedy",
+        reviews: {}
+      },
+      {
+        isbn: "4",
+        author: "Unknown",
+        title: "The Epic Of Gilgamesh",
+        reviews: {}
+      },
+      {
+        isbn: "5",
+        author: "Unknown",
+        title: "The Book Of Job",
+        reviews: {}
+      },
+      {
+        isbn: "6",
+        author: "Unknown",
+        title: "One Thousand and One Nights",
+        reviews: {}
+      },
+      {
+        isbn: "7",
+        author: "Unknown",
+        title: "Njál's Saga",
+        reviews: {}
+      },
+      {
+        isbn: "8",
+        author: "Jane Austen",
+        title: "Pride and Prejudice",
+        reviews: {}
+      },
+      {
+        isbn: "9",
+        author: "Honoré de Balzac",
+        title: "Le Père Goriot",
+        reviews: {}
+      },
+      {
+        isbn: "10",
+        author: "Samuel Beckett",
+        title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+        reviews: {}
+      }
+    ];
+    
+    module.exports = books;
+    
